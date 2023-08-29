f596490cdfc69967ec8f71716e5428552ad6b856
zingworks-kuldeep <kuldip.rajput@zingworks.in>
2023-08-29 12:43:21 +0530

    parameter changes.

--- lib/firebase_auth_utility.dart
+++ lib/firebase_auth_utility.dart
@@ -3,7 +3,7 @@ library firebase_auth_utility;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseAuthUtil {
@@ -138,18 +138,18 @@ class FirebaseAuthUtil {
     return '';
   }
 
-  // SignIn With Meta
-  Future<UserCredential> signInWithMeta() async {
-    final LoginResult loginResult = await FacebookAuth.instance
-        .login(permissions: ['email', 'public_profile', 'user_birthday']);
-
-    // Create a credential from the access token
-    final OAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.token);
-
-    // Once signed in, return the UserCredential
-    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-  }
+  // // SignIn With Meta
+  // Future<UserCredential> signInWithMeta() async {
+  //   final LoginResult loginResult = await FacebookAuth.instance
+  //       .login(permissions: ['email', 'public_profile', 'user_birthday']);
+  //
+  //   // Create a credential from the access token
+  //   final OAuthCredential facebookAuthCredential =
+  //       FacebookAuthProvider.credential(loginResult.accessToken!.token);
+  //
+  //   // Once signed in, return the UserCredential
+  //   return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  // }
 
   // SignIn with Apple
   signInWithApple() async {
@@ -159,5 +159,19 @@ class FirebaseAuthUtil {
     } on Exception catch (e) {
       return 'error in apple login -> $e';
     }
+  }
+
+  // Login with Microsoft
+  signInWithMicrosoft() async {
+    final MicrosoftAuthProvider microsoftProvider = MicrosoftAuthProvider();
+    microsoftProvider.addScope("openid");
+    try {
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithProvider(microsoftProvider);
+      return userCredential;
+    } on FirebaseAuthException catch (exception) {
+      debugPrint("exception->${exception.message}");
+    }
+    return '';
   }
 }
